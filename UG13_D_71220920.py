24ec35ba044775d3eebf1e619271ad484f6c47b2
Rio-arbanianto <117901421+Rio-arbanianto@users.noreply.github.com>
2022-12-15 16:29:22 +0700

    Add files via upload

---
+++ UG13_D_71220920.py
@@ -0,0 +1,35 @@
+print ("Nama: Mama Rio Arbanianto, Huruf yang dipakai adalah R,A")
+print("NIM: 71220920, Angka yang dipake adalah 920")
+
+import turtle as t
+t.bgcolor("blue")
+t.pensize(10)
+t.color("white")
+
+t.penup()
+t.goto(0,100)
+t.pendown()
+t.circle(80,180)
+t.left(90)
+t.forward(290)
+t.back(110)
+t.left(45)
+t.forward(150)
+t.left(45)
+
+t.penup()
+t.goto(0,-200)
+t.pendown()
+t.pensize(10)
+t.color("white")
+
+t.forward(130)
+t.right(45)
+t.forward(50)
+t.back(150)
+
+t.right(90)
+t.forward(150)
+
+
+
